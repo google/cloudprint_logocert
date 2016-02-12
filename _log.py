85b12d58589f00dcf954f31c27f2c2bedba25e3a
Kelly Lucas <kdlucas@gmail.com>
2016-02-12 09:56:02 -0800

    Setting loglevel and stdout to previous.

--- _log.py
+++ _log.py
@@ -28,7 +28,7 @@ import sys
 import time
 
 
-def GetLogger(name, logdir=None, loglevel='debug', stdout=True):
+def GetLogger(name, logdir=None, loglevel='info', stdout=False):
   """Return a new logger, or reference to an existing logger.
 
   Args:
